7adf2e18a3b5c9a948ec8887a573bc5b816b0907
Jessica Greene <jessica0greene@gmail.com>
2017-10-05 16:32:02 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
-//define functions here
+function getIt() {
+  $('p').on('click', function(){
+    alert("Hey!")
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').append('<style class="tasty"></style>')
+  })
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(e){
+    const key = parseInt(e.detail || e.which);
+    if (key ==== 71) {
+      alert("Your using the G key!")
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function(){
+    alert("Your form is going to be submitted now.")
+  })
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+  getIt()
+  frameIt()
+  pressIt()
+  submitIt()
 });
